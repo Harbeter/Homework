45e10a0930d92fe59db5c0ec33ce0dbf6b964c2c
‘Harbeter’ <qq2300350627@163.com>
2022-03-07 17:30:34 +0800

    test

---
+++ HT/画o/t1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i=0;
+	cout<<"   ***   "<<endl;
+	cout<<" *     * "<<endl;
+	for(i=0;i<5;i++)
+	{
+		cout<<"*       *"<<endl;
+	}
+	cout<<" *     * "<<endl;
+	cout<<"   ***   "<<endl;
+} 
